34df823c9768ebb48ec66354bdc6b16b1b626a2f
THDMoritzEnderle <private@moritzenderle.com>
2022-06-28 23:31:16 +0200

    Removed trash training data

--- sign_language/data_analysis.py
+++ sign_language/data_analysis.py
@@ -1,9 +1,9 @@
 import os
-
+import numpy as np
 from matplotlib import pyplot as plt
-from utils import *
 import seaborn as sns
 import pandas as pd
+from multiprocessing import Pool, freeze_support
 
 
 def set_color(_fig, _ax):
@@ -21,37 +21,47 @@ def set_color(_fig, _ax):
     _ax.tick_params(axis='y', colors='white')
 
 
-for folder in os.listdir(os.path.join(DATA_PATH)):
-    if not os.path.exists(os.path.join(f"../pictures/{folder}")):
-        os.makedirs(f"../pictures/{folder}")
+def save_plot(path):
+    fig, ax = plt.subplots(ncols=1, figsize=(10, 8), dpi=75)
+    try:
+        data = np.load(path)
+    except ValueError:
+        print(path)
+        return
+    xs = []
+    ys = []
+    color = []
+    for index, frame in enumerate(data):
+        for x in frame[0::2]:
+            xs.append(x)
+            color.append(index)
+        for y in frame[1::2]:
+            ys.append(1 - y)
+    df = pd.DataFrame({"x": xs, "y": ys, "color": color})
+    sns.scatterplot(x="x", y="y", hue="color", palette=sns.color_palette("hls", 20), data=df, ax=ax, alpha=0.1,
+                    legend=False)
+    set_color(fig, ax)
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    plt.axis('off')
+    plt.savefig(f"../pictures/{path.split('/')[-2]}/{path.split('/')[-1][:-4]}.png", bbox_inches='tight', pad_inches=0)
+    plt.close()
 
-    for seq in os.listdir(os.path.join(DATA_PATH, folder)):
-        if os.path.exists(os.path.join(f"../pictures/{folder}/{seq[:-4]}.png")):
+
+if __name__ == '__main__':
+    freeze_support()
+    for folder in os.listdir(os.path.join("../data")):
+
+        # check if it is an actual folder
+        if not os.path.isdir(os.path.join("../data", folder)):
             continue
-        print(folder, seq, sep="/")
-        fig, ax = plt.subplots(ncols=1, figsize=(10, 8), dpi=300)
-        data = np.load(os.path.join("../data/", str(folder), str(seq)))
-        xs = []
-        ys = []
-        color = []
-        for index, frame in enumerate(data):
-            for x in frame[0::2]:
-                xs.append(x)
-                color.append(index)
-            for y in frame[1::2]:
-                ys.append(1 - y)
 
-        # make dataframe with x and y coordinates
-        df = pd.DataFrame({"x": xs, "y": ys, "color": color})
-        # plot x and y as seaborn scatter plot
-        sns.scatterplot(x="x", y="y", hue="color", palette=sns.color_palette("hls", 20), data=df, ax=ax, alpha=0.1, legend=False)
+        if not os.path.exists(os.path.join(f"../pictures/{folder}")):
+            os.makedirs(f"../pictures/{folder}")
 
-        set_color(fig, ax)
-        # set x and y limits to 0 and 1
-        ax.set_xlim(0, 1)
-        ax.set_ylim(0, 1)
+        paths = [os.path.join(f"../data/{folder}/{seq}") for seq in os.listdir(os.path.join("../data", folder))]
+        print(paths)
+        with Pool(processes=16) as pool:
+            pool.map(save_plot, paths)
 
-        plt.axis('off')
-
-        # save the plot
-        plt.savefig(f"../pictures/{folder}/{seq[:-4]}.png", bbox_inches='tight', pad_inches=0)
+        print(f"{folder} done")
