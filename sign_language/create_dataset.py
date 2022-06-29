e737c4053cdc2c68130dc64abb18388c83c323fe
moritz <contact@moritzenderle.com>
2022-06-29 22:56:45 +0200

    updated to match current version

--- sign_language/create_dataset.py
+++ sign_language/create_dataset.py
@@ -1,10 +1,7 @@
-import cv2
-
 from utils import *
 
-actions = np.array(['project goals'])
-no_sequences = 10
-sequence_length = 20
+actions = np.array(['none'])
+no_sequences = 300
 
 create_folders(actions, DATA_PATH)
 
@@ -19,18 +16,17 @@ with mp_holistic.Holistic(min_detection_confidence=0.8, min_tracking_confidence=
             cv2.putText(frame, '{}, press c to start'.format(action), (120, 200),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4, cv2.LINE_AA)
             cv2.imshow('OpenCV Feed', frame)
-        cv2.waitKey(1000)
-        for sequence in range(no_sequences):
+
+        for sequence in range(320, 400):
 
             numpy_seq = []
-            """
             if sequence != 0 and sequence % 40 == 0:
                 while not cv2.waitKey(10) & 0xFF == ord('c'):
                     ret, frame = cap.read()
-                    cv2.putText(image, 'Take a break. press c to continue', (120, 200),
+                    cv2.putText(frame, 'Take a break. press c to continue', (120, 200),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4, cv2.LINE_AA)
                     cv2.imshow('OpenCV Feed', frame)
-            """
+
             while True:
 
                 if len(numpy_seq) >= sequence_length:
@@ -39,6 +35,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.8, min_tracking_confidence=
                 ret, frame = cap.read()
                 image, results = detect_landmarks(frame, holistic)
                 draw_landmarks(image, results)
+
                 if len(numpy_seq) == 0:
                     cv2.putText(image, 'STARTING COLLECTION', (120, 200),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
@@ -50,11 +47,12 @@ with mp_holistic.Holistic(min_detection_confidence=0.8, min_tracking_confidence=
                     cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     cv2.imshow('OpenCV Feed', image)
-                    cv2.waitKey(10)
 
-                if results.right_hand_landmarks or results.left_hand_landmarks:
+                if True or results.right_hand_landmarks or results.left_hand_landmarks:
                     key_points = create_numpy(results)
                     numpy_seq.append(key_points)
+
+                cv2.waitKey(10)
 
             npy_path = os.path.join(DATA_PATH, action, str(sequence))
             np.save(npy_path, np.array(numpy_seq))
